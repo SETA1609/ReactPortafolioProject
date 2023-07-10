5f922eaad513fd0d60419af576635e9160f1cdc0
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-07-10 11:44:03 +0200

    ft: complete custom change without chakra

---
+++ ReactPortafolioProject/src/App.test.tsx
@@ -0,0 +1,16 @@
+import App from "./App";
+import{describe,expect,it}from"vitest";
+
+describe("description", ()=>{
+it("description", ()=>{
+expect(function()).toBe(result)
+})
+})
+
+/**
+ * describe("description", ()=>{
+it("description", ()=>{
+expect(function()).toBe(result)
+})
+})
+ */
---
+++ ReactPortafolioProject/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import"./Navbar.css";
+function Navbar() {
+    return (
+        <header>
+            <nav>
+                navbar
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;

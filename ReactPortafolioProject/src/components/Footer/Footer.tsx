6ed2b2ff95ff90e4e4bbd1002ae23d328ad523b2
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-07-10 14:29:19 +0200

    ft vitest config was setup and some asset

--- ReactPortafolioProject/src/components/Footer/Footer.tsx
+++ ReactPortafolioProject/src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import"./Footer.css";
 
-function Footer(props) {
+function Footer() {
     return (
-        <div>
-
-        </div>
+        <footer>
+            <p> footer </p>
+        </footer>
     );
 }
 

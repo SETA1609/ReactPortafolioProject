fb59c293dd9f303967aa6513c090776e4fd2ae41
SETA1609 <sebastian.stp16@gmail.com>
2023-08-15 21:51:30 +0200

    ft: LandingPage.tsx is done

--- ReactPortafolioProject/src/components/LandingPage/LandingPage.tsx
+++ ReactPortafolioProject/src/components/LandingPage/LandingPage.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
-import"./LandingPage.css";
+import "./LandingPage.css";
 import photo from "./foto.png";
+
 function LandingPage() {
     return (
         <div className='landingPage'>
-       
-		<img src={photo} alt="My Photo" />    	
-
-	</div>
+            <div className="foto-Container">
+                <img src={photo} alt="My Photo"  className="foto" />
+            </div>
+            <p>
+                Hi, Im Sebastian!
+            </p>
+            <h1>
+                A fullstack developer specialized in Springboot, React, Angular and Django
+            </h1>
+        </div>
     );
 }
 

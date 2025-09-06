1b193084e8c9fb706581e0d783998a2a7565a0a1
Sebastian Tamayo <sebastian.tamayo@whereversim.de>
2025-09-06 07:11:30 +0200

    somes changes on the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -16,78 +16,80 @@ const Navbar: React.FC = () => {
   const setLanguage = useSetLanguage();
 
   return (
-    <header className={`bg-${isDarkTheme ? 'dark' : 'light'} text-${isDarkTheme ? 'light' : 'dark'}`}>
-      <nav className="navbar navbar-expand-lg container py-3">
-        <div className="container-fluid">
-          <ul className="navbar-nav me-auto mb-0 align-items-center flex-row flex-nowrap">
-            {Icons.map((icon, index) => (
-              <li key={index} className="nav-item me-3">
-                <a 
-		href={icon.url} 
-		target='_blank' 
-		className={`nav-link text-${isDarkTheme ? 'light' : 'dark'}`}
-		>
-                  <FontAwesomeIcon icon={icon.icon} />
+  <div ClassName="container">
+    <nav
+      className={`navbar navbar-${isDarkTheme ? 'dark' : 'light'}  
+      text-${isDarkTheme ? 'light' : 'dark'} navbar-expand-lg py-3`}>
+      <div className="container-fluid">
+        <ul className="navbar-nav me-auto mb-0 align-items-center flex-row flex-nowrap">
+          {Icons.map((icon, index) => (
+            <li key={index} className="nav-item me-3">
+              <a
+                href={icon.url}
+                target="_blank"
+                className={`nav-link text-${isDarkTheme ? 'light' : 'dark'}`}
+              >
+                <FontAwesomeIcon icon={icon.icon} />
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav mb-0 align-items-lg-center">
+            {InnerLinks.map((link, index) => (
+              <li key={index} className="nav-item me-1">
+                <a
+                  href={link.url}
+                  className={`btn btn-${isDarkTheme ? 'light' : 'dark'} w-100 w-lg-auto mx-1 my-1 my-lg-0`}
+                >
+                  {t(`nav.${link.key}`)}
                 </a>
               </li>
             ))}
+            <li className="nav-item ms-1">
+              <button
+                className={`btn btn-${isDarkTheme ? 'light' : 'dark'} w-100 w-lg-auto mx-1 my-1 my-lg-0`}
+                onClick={toggleTheme}
+              >
+                {t('nav.toggleTheme')}
+              </button>
+            </li>
+            <li className="nav-item dropdown">
+              <button
+                className={`btn btn-${isDarkTheme ? 'light' : 'dark'} dropdown-toggle w-100 w-lg-auto mx-1 my-1 my-lg-0`}
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                {t(`nav.language.${language}`)}
+              </button>
+              <ul className="dropdown-menu">
+                {languages.map(lang => (
+                  <li key={lang}>
+                    <button className="dropdown-item text-center" onClick={() => setLanguage(lang)}>
+                      {t(`nav.language.${lang}`)}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </li>
           </ul>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-            <ul className="navbar-nav mb-0 align-items-lg-center">
-              {InnerLinks.map((link, index) => (
-                <li key={index} className="nav-item">
-                  <a
-                    href={link.url}
-                    className={`btn btn-${isDarkTheme ? 'light' : 'dark'} mx-1 my-1 my-lg-0`}
-                  >
-                    {t(`nav.${link.key}`)}
-                  </a>
-                </li>
-              ))}
-              <li className="nav-item">
-                <button
-                  className={`btn btn-${isDarkTheme ? 'light' : 'dark'} mx-1 my-1 my-lg-0`}
-                  onClick={toggleTheme}
-                >
-                  {t('nav.toggleTheme')}
-                </button>
-              </li>
-              <li className="nav-item dropdown">
-                <button
-                  className={`btn btn-${isDarkTheme ? 'light' : 'dark'} dropdown-toggle mx-1 my-1 my-lg-0`}
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  {t(`nav.language.${language}`)}
-                </button>
-                <ul className="dropdown-menu">
-                  {languages.map(lang => (
-                    <li key={lang}>
-                      <button className="dropdown-item" onClick={() => setLanguage(lang)}>
-                        {t(`nav.language.${lang}`)}
-                      </button>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            </ul>
-          </div>
         </div>
-      </nav>
-    </header>
+      </div>
+    </nav>
+        </div>
   );
 };
 

86059c00c590e5fafb7777e3396d45843b816f32
Sebastian Tamayo <123449150+SETA1609@users.noreply.github.com>
2025-09-08 17:23:09 +0200

    test: add React import usage

--- src/components/ContactForm/ContactForm.test.tsx
+++ src/components/ContactForm/ContactForm.test.tsx
@@ -5,6 +5,8 @@ import ContactForm from './ContactForm';
 import { ThemeProvider } from '../../context/ThemeContext';
 import { LanguageProvider } from '../../context/LanguageContext';
 
+void React;
+
 jest.mock('react-i18next', () => ({
   useTranslation: () => ({ t: (key: string) => key })
 }));
